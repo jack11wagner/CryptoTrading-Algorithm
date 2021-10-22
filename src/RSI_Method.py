7027080bc49def0cec5e0670211ac3c8cf483240
jack11wagner <jacksonjw519@gmail.com>
2021-10-22 12:23:04 -0400

    Updated date time output from UTC time to NYEASTERN

--- src/RSI_Method.py
+++ src/RSI_Method.py
@@ -1,5 +1,6 @@
 import pandas as pd
-import websocket, json, pprint, talib, numpy, pandas, pymysql, os
+import websocket, json, pprint, talib, numpy, pandas, pymysql, os, datetime
+from pytz import timezone
 from dotenv import load_dotenv
 import mysql.connector
 from sqlalchemy import create_engine
@@ -18,21 +19,47 @@ engine = create_engine('mysql+pymysql://{}:{}@localhost/crypto_db'.format(user, 
 
 client = Client(API_KEY, API_SECRET, tld='us')
 
+# DEFINING PARAMETERS FOR TRADING
 RSI_PERIOD = 14
 RSI_OVERBOUGHT = 70
 RSI_OVERSOLD = 30
 TRADE_SYMBOL = 'BTCUSD'
 TRADE_QUANTITY = 0.000384
+TIMEZONE = timezone('America/New_York')
+
+
+def loadCandleCloses(symbol, Time, openprice, closeprice, change):
+    curr_time = datetime.datetime.now(TIMEZONE)
+    df = pd.DataFrame({'symbol': [symbol], 'Time': [curr_time], 'Open_Price': [openprice], 'Close_Price': [closeprice],
+                       'Change_in_Price': change})
+
+    df.Time = pd.to_datetime(df.Time, unit='ms')
+    return df
+
+
+def loadRSI(Symbol, Time, RSI):
+    curr_time = datetime.datetime.now(TIMEZONE)
+    df = pd.DataFrame({'symbol': [Symbol], 'Time': [curr_time], 'LastRSI': [RSI]})
+    df.to_sql('RSI', engine, if_exists='append', index=False)
+
+
+def loadTransactions(order, side):
+    price = float(order['fills'][0]['price'])
+    curr_time = datetime.datetime.now(TIMEZONE)
+    quantity = float(order['origQty'])
+    dollar_AMT = quantity * price
+    order_data = {'Time': [curr_time],
+                  'Type': [side], 'Price': [price],
+                  'Quantity': [quantity], 'DollarAMT': [dollar_AMT]}
+    buy_df2 = pd.DataFrame.from_dict(order_data)
+    print(buy_df2)
+    buy_df2.to_sql('Transactions', engine, if_exists='append', index=False)
 
 
 def order(side, quantity, symbol, order_type=ORDER_TYPE_MARKET):
     try:
         order = client.create_order(symbol=symbol, side=side, type=ORDER_TYPE_MARKET, quantity=quantity)
-        order_data = {'Time': [pd.to_datetime(order['transactTime'], unit='ms')],
-                      'Type': [side], 'Price': [float(order['fills'][0]['price'])],
-                      'Quantity': [float(order['origQty'])]}
-        buy_df2 = pd.DataFrame.from_dict(order_data)
-        buy_df2.to_sql('Transactions', engine, if_exists='append', index=False)
+        loadTransactions(order, side)
         print('sending order')
         print(order)
 
@@ -42,43 +69,24 @@ def order(side, quantity, symbol, order_type=ORDER_TYPE_MARKET):
     return True
 
 
-def createFrame(symbol, Time, openprice, closeprice, change):
-    df = pd.DataFrame({'symbol': [symbol], 'Time': [Time], 'Open_Price': [openprice], 'Close_Price': [closeprice],
-                       'Change_in_Price': change})
-
-    df.Time = pd.to_datetime(df.Time, unit='ms')
-    return df
-
-
-def RSI_Frame_To_SQL(Symbol, Time, RSI):
-    df = pd.DataFrame({'symbol': [Symbol], 'Time': [pd.to_datetime(Time, unit='ms')], 'LastRSI': [RSI]})
-    df.to_sql('RSI', engine, if_exists='append', index=False)
-
-
 SOCKET = 'wss://stream.binance.com:9443/ws/btcusdt@kline_1m'
 
-
-def on_open(ws):
-    print('opened connection')
-
-
-def on_close(ws):
-    print('closed connection')
-
-
 close_data = pd.read_sql('CandleCloses', engine)
+
+# GETTING CLOSE DATA
 closes = list(close_data['Close_Price'])
 print(closes)
-last_position = pd.read_sql('Transactions', engine)
 
+# CHECKING SQL TO SEE IF LAST REGISTERED TRANSACTION WAS BUY OR SELL
+last_position = pd.read_sql('Transactions', engine)
 if last_position['Type'].iloc[-1] == 'BUY':
     in_position = True
-    print('In current position at $',last_position['Price'].iloc[-1], 'with quantity of', last_position['Quantity'].iloc[-1])
+    print('In current position at $', last_position['Price'].iloc[-1], 'with quantity of',
+          last_position['Quantity'].iloc[-1])
+    print('Current', TRADE_SYMBOL, 'price $', closes[-1])
 else:
     in_position = False
     print('Not currently in a position')
-
-
 
 
 def on_message(ws, message):
@@ -95,14 +103,14 @@ def on_message(ws, message):
     change = float(close) - float(open)
 
     if is_candle_closed:
-        print('candle closed at {}'.format(close))
+        print('candle closed at {}'.format(float(close)))
 
         closes.append(float(close))
-        frame = createFrame(symbol, time, float(open), float(close), float(change))
+        frame = loadCandleCloses(symbol, time, float(open), float(close), float(change))
         print(frame)
         frame.to_sql('CandleCloses', engine, if_exists='append', index=False)
-        print('closes')
-        print(closes)
+        # print('closes')
+        # print(closes)
 
         # makes sure number of closes recorded is greater than 14
         if len(closes) > RSI_PERIOD:
@@ -111,7 +119,7 @@ def on_message(ws, message):
             print("all rsis calculated so far")
             print(rsi)
             last_rsi = rsi[-1]
-            RSI_Frame_To_SQL(symbol, time, last_rsi)
+            loadRSI(symbol, time, last_rsi)
 
             print('the current rsi is {}'.format(last_rsi))
 
@@ -135,5 +143,14 @@ def on_message(ws, message):
                         in_position = True
 
 
+def on_open(ws):
+    print('opened connection')
+
+
+def on_close(ws):
+    print('closed connection')
+
+
 ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
+
 ws.run_forever()
